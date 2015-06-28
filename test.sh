52741ca093b1ff109dba3dd0976fbc4fe543d463
arkady <larytet@yahoo.com>
2015-06-28 22:23:22 +0300

    Minor fix in debug print in test.sh

--- test.sh
+++ test.sh
@@ -5,13 +5,13 @@ WORK_DIR=`dirname $0`
 echo Run GTBBTR
 $WORK_DIR/task1.py pipeline --stages GTBBTR
 
-echo Run GTBBTR
+echo Run GTPPTR
 $WORK_DIR/task1.py pipeline --stages GTPPTR
 
-echo Run GTBBTR
+echo Run GTPBTR
 $WORK_DIR/task1.py pipeline --stages GTPBTR
 
-echo Run GTBBTR
+echo Run GTBPTR
 $WORK_DIR/task1.py pipeline --stages GTBPTR
 
 
