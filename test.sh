83fc525f930c10b00cd11d2cba934e652dee80bd
arkady <larytet@yahoo.com>
2015-06-28 19:46:38 +0300

    Testing shell

--- test.sh
+++ test.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 WORK_DIR=`dirname $0`
+
+echo Run GTBBTR
 $WORK_DIR/task1.py pipeline --stages GTBBTR
+
+echo Run GTBBTR
 $WORK_DIR/task1.py pipeline --stages GTPPTR
+
+echo Run GTBBTR
 $WORK_DIR/task1.py pipeline --stages GTPBTR
+
+echo Run GTBBTR
 $WORK_DIR/task1.py pipeline --stages GTBPTR
+
 
 
